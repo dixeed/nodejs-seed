a2ed8a9b542a5acdbed2439b8d9abe54a5f965b5
Timothe BORDIGA <t.bordiga@dixeed.com>
2016-02-12 16:28:38 +0100

    Create index.js
    Add the standard index.js file to use for NodeJS development.

---
+++ index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const config = require('./config');
+const setDatabaseServerExtensions = require('./config/database/extension');
+
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+module.exports = server;
+
+/////////////////////////////////////////////////////////////////////////
+
+server.connection({
+    host: config.get('/server/host'),
+    port: config.get('/server/port')
+});
+
+setDatabaseServerExtensions(server);
+
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//                      Plugins registration                      //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+server.register([
+    {
+        register: require('hapi-sequelize'),
+        options: {
+            database: config.get('/database/credentials/dbName'),
+            user: config.get('/database/credentials/user'),
+            pass: config.get('/database/credentials/pass'),
+            dialect: config.get('/database/credentials/dialect'),
+            host: config.get('/database/credentials/host'),
+            port: config.get('/database/credentials/port'),
+
+            models: 'lib/**/model.js',
+            logging: false // should be a function if want to log
+        }
+    },
+    require('hapi-auth-jwt2'),
+    require('./lib/auth-jwt'),
+    require('./lib/user')
+])
+
+/////////////////////////////////////////////////////////////////////////
+//                                                                     //
+//                      Plugins configuration                          //
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
+.then(function () {
+    var db = server.plugins['hapi-sequelize'].db;
+
+    // Reload the database when the server is restarted (only in dev mode).
+    return db.sequelize.sync({
+        force: config.get('/database/syncForce')
+    });
+})
+.catch(function (err) {
+    console.error('Failed to load a plugin: ', err);
+    process.exit(2);
+})
+
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//                      Start the server                          //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+.then(function() {
+    // Success callback for the db.sequelize.sync() function call above.
+    console.log('Models synced: [' + 'OK' + ']');
+    return server.start();
+})
+.then(function() {
+    console.log('Server started on port : [' + config.get('/server/port') + ']');
+})
+.catch(function(err) {
+    console.error('Started server with errors: ' + err);
+    process.exit(3);
+});
